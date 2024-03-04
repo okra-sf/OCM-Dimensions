d13f6dde79673024100c3a122528a0b050c753cc
Ramana Rao <treerao@gmail.com>
2024-03-04 06:08:19 +0000

    update builds for localdev ease

--- p5js/build.js
+++ p5js/build.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const page_structure = fs.readFileSync('page-structure.html', {encoding:'utf8'});
-const src_compressed = fs.readFileSync('src-compressed-string.base64', {encoding:'utf8'}).trim()
+var src = process.argv[2];
+const page_structure = fs.readFileSync('page-structure2.html', {encoding:'utf8'});
+const src_compressed = fs.readFileSync(src, {encoding:'utf8'}).trim()
 const page01 = page_structure.replace('SRC_COMPRESSED_STRING', src_compressed);
 console.log(page01);
